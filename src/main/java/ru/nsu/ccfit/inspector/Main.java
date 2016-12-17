273909dcd4091ef2910a9ba60137dcfd2e6fd6d6
boyarin@gmail.com <boyarintsev95@gmail.com>
2016-12-17 10:13:55 +0700

    changes

---
+++ src/main/java/ru/nsu/ccfit/inspector/Main.java
@@ -0,0 +1,7 @@
+package ru.nsu.ccfit.inspector;
+
+/**
+ * Created by artem on 17.12.16.
+ */
+public class Main {
+}
---
+++ src/main/java/ru/nsu/ccfit/inspector/checkers/SelectChecker.java
@@ -0,0 +1,7 @@
+package ru.nsu.ccfit.inspector.checkers;
+
+/**
+ * Created by artem on 17.12.16.
+ */
+public class SelectChecker {
+}
